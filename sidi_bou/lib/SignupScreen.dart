33360524b01b30dc1336e93047349dd15e02664a
Ines Khelil <khelilines14@gmail.com>
2024-04-17 00:48:00 +0200

    my commit

---
+++ sidi_bou/lib/SignupScreen.dart
@@ -0,0 +1,185 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+  Future SignUp() async {
+    if (passwordConfirmed()) {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      );
+      // Navigator.of(context).pushNamed('/');
+    }
+  }
+
+  bool passwordConfirmed() {
+    if (_passwordController.text.trim() ==
+        _confirmPasswordController.text.trim()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  // void openLoginScreen() {
+  //   Navigator.of(context).pushReplacementNamed('LoginScreen');
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue[700],
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //image
+                Image.asset(
+                  'images/mascotte.png',
+                  height: 150,
+                ),
+                SizedBox(height: 10),
+
+                //Title
+                Text(
+                  'SIGN UP',
+                  style: GoogleFonts.robotoCondensed(
+                      fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+                //Subtitle
+                Text(
+                  'Get Started with Sidi Bou App !',
+                  style: GoogleFonts.robotoCondensed(fontSize: 18),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                //EMAIL
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12)),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: TextField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                            border: InputBorder.none, hintText: 'Email'),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                // Password
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12)),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: TextField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            border: InputBorder.none, hintText: 'Password'),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                // Confirm Password
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12)),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: TextField(
+                        controller: _confirmPasswordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Confirm Password'),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                //Button SignUP
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: GestureDetector(
+                    onTap: SignUp,
+                    child: Container(
+                      padding: EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: Colors.red[900],
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Center(
+                          child: Text(
+                        'Sign up',
+                        style: GoogleFonts.robotoCondensed(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      )),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+
+                //TEXT ALREADY A MEMBER
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Already a member? ',
+                      style: GoogleFonts.robotoCondensed(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    GestureDetector(
+                        // onTap: openLoginScreen,
+                        child: Text(
+                      'Sign in here',
+                      style: GoogleFonts.robotoCondensed(
+                        color: Colors.red[500],
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
---
+++ sidi_bou/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:sidi_bou/auth.dart';
+import 'package:sidi_bou/firebase_options.dart';
+import './SignupScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sidi Bou Said',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SignupScreen(),
+      routes: {
+        // '/': (context) => const Auth(),
+        // 'HomeScreen': (context) => const HomeScreen(),
+        // 'SignUpScreen': (context) => const SignupScreen(),
+        // 'LoginScreen': (context) => const LoginScreen(),
+      },
+    );
+  }
+}
